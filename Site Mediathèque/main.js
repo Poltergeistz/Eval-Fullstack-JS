285389d18beeb8b307c5158bb05b606828850897
david <david.journo.31@gmail.com>
2018-06-25 15:34:11 +0200

    DOM manipulation for BOOKS API

--- Site Mediathèque/main.js
+++ Site Mediathèque/main.js
@@ -1,2 +1,21 @@
 // API Google Books
-var apibooks ="" ;
+var apibooks ="" ; // AIzaSyDryhDj129-9EgUtLOqE3zFvIkuSH3Tx3Y
+
+// DOM
+var btn = document.getElementById('btn');
+var search = document.getElementById('search').value;
+document.getElementById('results').innerHTML = "";
+
+// On click display the research
+btn.addEventListener('click', function (){
+    var query = new XMLHttpRequest();
+    query.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=' + search);
+    query.onload(function(){
+        var bookdata = JSON.parse(query.responseText);
+        console.log(bookdata);
+    });
+});
+
+function searchBook(){
+
+}
