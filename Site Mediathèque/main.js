ac8569cb1629bc8852e0ae36ef70cd80943fe61b
david <david.journo.31@gmail.com>
2018-06-25 16:17:05 +0200

    research function

--- Site Mediathèque/main.js
+++ Site Mediathèque/main.js
@@ -3,21 +3,19 @@ var apibooks ="" ; // AIzaSyDryhDj129-9EgUtLOqE3zFvIkuSH3Tx3Y
 
 // DOM
 var btn = document.getElementById('btn');
-
+var results = document.getElementById('results')
 // On click display the research
 btn.addEventListener('click', function (){
     var request = new XMLHttpRequest();
     var search = document.getElementById('search').value;
     request.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=' + search);
-    request.onload = function(){
+    request.onload = function(data){
         var bookdata = JSON.parse(request.responseText);
         console.log(bookdata);
+        for (var i = 0; i < data.items.length; i++){
+            var item = data.items[i];
+            results.innerHTML += "<br>" + item;
+        }
     };
     request.send();
 });
-
-function searchBook(){
-    
-    document.getElementById('results').innerHTML = "";
-    console.log(search);
-}
