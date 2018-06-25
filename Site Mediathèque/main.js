04e2dc30aab224624617212459ecac2b1c2ca2d8
david <david.journo.31@gmail.com>
2018-06-25 16:37:00 +0200

    API key

--- Site Mediathèque/main.js
+++ Site Mediathèque/main.js
@@ -1,21 +1,22 @@
 // API Google Books
-var apibooks ="" ; // AIzaSyDryhDj129-9EgUtLOqE3zFvIkuSH3Tx3Y
+var search = document.getElementById('search').value;
+var apibooks ="https://www.googleapis.com/books/v1/volumes?q="+search+"&key=AIzaSyDryhDj129-9EgUtLOqE3zFvIkuSH3Tx3Y" ; // AIzaSyDryhDj129-9EgUtLOqE3zFvIkuSH3Tx3Y
 
 // DOM
 var btn = document.getElementById('btn');
-var results = document.getElementById('results')
+var results = document.getElementById('results');
+
 // On click display the research
 btn.addEventListener('click', function (){
     var request = new XMLHttpRequest();
-    var search = document.getElementById('search').value;
-    request.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=' + search);
-    request.onload = function(data){
-        var bookdata = JSON.parse(request.responseText);
-        console.log(bookdata);
-        for (var i = 0; i < data.items.length; i++){
+    request.open('GET', apibooks);
+    request.onload = function(){
+        var data = JSON.parse(request.responseText);
+        console.log(data);
+        /* for (var i = 0; i < data.items.length; i++){
             var item = data.items[i];
-            results.innerHTML += "<br>" + item;
-        }
+            results.innerHTML += "<br>" + item.volumeInfo.title;
+        } */
     };
     request.send();
 });
