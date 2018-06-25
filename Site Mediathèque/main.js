e3d12b6e3f69797bd0c2f3092c8aca055d84cc98
david <david.journo.31@gmail.com>
2018-06-25 15:44:49 +0200

    JSHint

--- Site Mediathèque/main.js
+++ Site Mediathèque/main.js
@@ -3,19 +3,20 @@ var apibooks ="" ; // AIzaSyDryhDj129-9EgUtLOqE3zFvIkuSH3Tx3Y
 
 // DOM
 var btn = document.getElementById('btn');
-var search = document.getElementById('search').value;
-document.getElementById('results').innerHTML = "";
 
 // On click display the research
 btn.addEventListener('click', function (){
-    var query = new XMLHttpRequest();
-    query.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=' + search);
-    query.onload(function(){
-        var bookdata = JSON.parse(query.responseText);
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=' + search);
+    request.onload = function(){
+        var bookdata = JSON.parse(request.responseText);
         console.log(bookdata);
-    });
+    };
+    request.send();
 });
 
 function searchBook(){
-
+    var search = document.getElementById('search').value;
+    document.getElementById('results').innerHTML = "";
+    console.log(search);
 }
